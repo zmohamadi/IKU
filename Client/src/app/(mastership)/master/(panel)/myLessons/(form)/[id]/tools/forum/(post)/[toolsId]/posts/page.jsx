dc3e94fe0fcf4c006e5352c38e5eb65dcca0c4db
zmohamadi <zmohamadi44@yahoo.com>
2024-06-08 10:57:29 +0430

    my lessons

---
+++ Client/src/app/(mastership)/master/(panel)/myLessons/(form)/[id]/tools/forum/(post)/[toolsId]/posts/page.jsx
@@ -0,0 +1,9 @@
+"use client";
+import { useConfig } from "@/lib/config";
+import { Page } from "@/Components/Forum/Page"
+
+export default function List({params}){
+    const {laraAdmin ,nextAdmin } = useConfig();
+
+    return <Page toolsId={params.toolsId} lesson={params.id} laraPath={laraAdmin} nextPath={nextAdmin} access={true} />
+}
